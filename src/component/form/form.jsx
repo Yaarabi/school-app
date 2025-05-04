34f33e5572279305f8a63347e67d41696dc55b24
Yaarabi <yaarabi318@gmail.com>
2025-05-04 17:42:16 +0100

    add- the form of login to the other pages and use axios to fetch data

---
+++ src/component/form/form.jsx
@@ -0,0 +1,66 @@
+
+import { useState, useContext, useEffect} from "react"
+import { useNavigate } from "react-router-dom"
+import "./form.css"
+import { ForLogin } from "../home"
+import axios from "axios"
+
+
+const Form = () => {
+    
+    let [user, setUser]=useState([])
+    let {student, setLogin, teacher} = useContext(ForLogin);
+    let [name, setName]=useState('')
+    let [pass, setPass]=useState('')
+    const navegate = useNavigate()
+
+    let hiden = ()=> setLogin(false)
+    
+    let loginToTeacher = () =>{
+        (name=="admin" && pass=="admin")? (navegate("/teach")) : (alert("your input incorrect"))
+    }
+
+    useEffect(()=>{
+        axios.get("/src/db.json")
+        .then((resp)=>{
+            setUser(resp.data)
+            console.log(resp.data)
+        })
+    }, [])
+
+    let loginToStud = () =>{
+        (user.find((ele)=>(ele.email==name)) && user.find((ele)=>(ele.username==pass)))
+        ? (navegate("/stud")) 
+        : (alert("your input incorrect"))
+    }
+
+
+return (
+    
+    <div id='form'>
+        <div>
+            <label>User Name:</label>
+            <input 
+            type="text"
+            value={name}
+            onChange={(e)=>setName(e.target.value)}
+            placeholder="your user name"/>
+        </div>
+        <div>
+            <label>Password:</label>
+            <input 
+            type="password"
+            value={pass}
+            onChange={(e)=>setPass(e.target.value)}
+            placeholder="your password"/>
+        </div>
+        {teacher && <button onClick={loginToTeacher} >Login</button>}
+        {student && <button onClick={loginToStud} >Login</button>}
+        <img onClick={hiden} src="/icon/close.png" height={"20px"} alt="close icon" />
+    </div>
+    
+)
+}
+
+export default Form
+
--- src/component/home/index.jsx
+++ src/component/home/index.jsx
@@ -1,10 +1,30 @@
 
+import { useState, createContext } from "react"
 import "./style.css"
 import React from 'react'
+import Form from "../form/form"
+
+const ForLogin = createContext()
 
 const Home = () => {
+
+    let [login, setLogin]=useState(false)
+    let [student, setStudent]=useState(false)
+    let [teacher, setTeacher]=useState(false)
+
+    let toTeacher = ()=>{
+        setLogin(true)
+        setTeacher(true)
+        setStudent(false)
+    }
+    let toStud = ()=>{
+        setLogin(true)
+        setStudent(true)
+        setTeacher(false)
+    }
+
 return (
-    <>
+    <ForLogin.Provider value={{student, setLogin, teacher}}>
     <nav>
         <h1>One Place One School</h1>
         <div className="loginSection">
@@ -17,12 +37,14 @@ return (
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus at iste deserunt praesentium suscipit ducimus adipisci delectus molestias veniam, obcaecati dignissimos modi nulla laudantium dolore? Neque veniam assumenda provident quis.
         </p>
         <section>
-            <div>Teacher</div>
-            <div>Student</div>
+            <div onClick={toTeacher}>Teacher</div>
+            <div onClick={toStud}>Student</div>
         </section>
+        {login && <Form/>}
     </main>
-    </>
+    </ForLogin.Provider>
 )
 }
 
 export default Home
+export {ForLogin}
